b4a6fd3f7e98a13d5b7ba253c0e5a503dfd57567
kaitlynjanerouse <96274087+kaitlynjanerouse@users.noreply.github.com>
2024-12-02 21:46:30 -0600

    fix map error

--- Code/preprocess.py
+++ Code/preprocess.py
@@ -73,7 +73,7 @@ class Preprocess():
                 transposed -= 12
             return transposed
 
-        df = df.map(transpose_and_wrap)
+        df = df.applymap(transpose_and_wrap)
         return df
 
 
